1f8b108042eecec53425bf29b6acced92aae0f62
konstantin <konst.gromov@gmail.com>
2026-05-26 01:59:46 +0300

    Add more verbosity with OneTimeTokenGenerationSuccessHandler implementation

--- src/main/java/org/kgromov/config/SecurityConfig.java
+++ src/main/java/org/kgromov/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package org.kgromov.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.ott.OneTimeToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authorization.EnableMultiFactorAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +16,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
+import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.util.Set;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -29,20 +37,15 @@ public class SecurityConfig {
 //    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) {
         return http
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/", "/ott/sent").permitAll()
+                        .anyRequest().authenticated())
                 .webAuthn(a -> a.allowedOrigins("http://localhost:8080/")
                         .rpName("kgromov")
                         .rpId("localhost")
                 )
                 .formLogin(withDefaults())
-                .oneTimeTokenLogin(withDefaults())
-                .oneTimeTokenLogin(ott ->
-                        ott.tokenGenerationSuccessHandler((request, response, oneTimeToken) -> {
-                            response.getWriter().println("you've got console mail!");
-                            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            IO.println("please go to http://localhost:8080/login/ott?token=" +
-                                    oneTimeToken.getTokenValue());
-                        }))
+                .oneTimeTokenLogin(_ -> new OttSuccessHandler())
                 .build();
     }
 
@@ -59,7 +62,8 @@ public class SecurityConfig {
                             response.setContentType(MediaType.TEXT_PLAIN_VALUE);
                             IO.println("please go to http://localhost:8080/login/ott?token=" +
                                     oneTimeToken.getTokenValue());
-                        }));
+                        })
+                );
     }
 
     @Bean
@@ -77,5 +81,30 @@ public class SecurityConfig {
                                 .build()
                 )
         );
+    }
+
+    @Bean
+    OneTimeTokenGenerationSuccessHandler ottSuccessHandler() {
+        return new OttSuccessHandler();
+    }
+
+    // More verbose implementation of OneTimeTokenGenerationSuccessHandler with redirection
+    private static class OttSuccessHandler implements OneTimeTokenGenerationSuccessHandler {
+        // this is a bit redundant, but it's just for the example.
+        // redirect can be removed and redirectHandler.handle not invoked - since it's required to permit "/ott/sent"
+        // default redirect - `ott/generate`
+        private final OneTimeTokenGenerationSuccessHandler redirectHandler = new RedirectOneTimeTokenGenerationSuccessHandler("/ott/sent");
+
+        @Override
+        public void handle(HttpServletRequest request, HttpServletResponse response, OneTimeToken oneTimeToken) throws IOException, ServletException {
+            String magicLink = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/login/ott")
+                    .queryParam("token", oneTimeToken.getTokenValue())
+                    .toUriString();
+            response.getWriter().println("you've got console mail!");
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            IO.println("please go to " + magicLink);
+            this.redirectHandler.handle(request, response, oneTimeToken);
+        }
     }
 }
